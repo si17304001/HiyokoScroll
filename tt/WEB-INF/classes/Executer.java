35bc02c5e576cf159130b5d6c96e4f3c4cfb39d8
si17304001 <35793632+si17304001@users.noreply.github.com>
2018-02-09 13:48:42 +0900

    tt

---
+++ tt/WEB-INF/classes/Executer.java
@@ -0,0 +1,12 @@
+public class Executer{]
+	
+	DBAccessor dba=new DBAccessor(); 
+	
+	void addBad(){}
+	void addGood(){}
+	void addRes(){}
+	void addThread(){}
+	void CreateAccount(String name, String pass){}
+	
+	
+}
